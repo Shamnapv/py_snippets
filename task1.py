df2c9dc0623a3e83e338f8eafefd227a1579ac5d
Shamnapv <161368598+Shamnapv@users.noreply.github.com>
2025-06-06 17:53:38 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,3 +9,13 @@ def arrsorting():
     print("array after sorting =",arr)
 arrsorting()
 
+"""output:
+(base) PS C:\Users\shaim\Desktop\pythonlab> python task1.py 
+enter the size of array5
+enetr element of the array1
+enetr element of the array10
+enetr element of the array8
+enetr element of the array2
+enetr element of the array9
+array before sorting= [1, 10, 8, 2, 9]
+array after sorting = [1, 2, 8, 9, 10]"""
