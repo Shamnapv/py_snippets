4453ba027900a7091b2ca7099eda1067fe25713b
Shamnapv <161368598+Shamnapv@users.noreply.github.com>
2025-06-06 17:56:21 +0530

    Update task2a.py

--- task2a.py
+++ task2a.py
@@ -4,3 +4,11 @@ print("hello world...", file=f, flush=True)
 x=10
 print(f"processing {x}", flush=True)
 f.close()
+
+"""output:
+    (base) PS C:\Users\shaim\Desktop\pythonlab> python task2a.py
+    processing 10
+
+    output.txt:
+      hii everyone
+      hello world..."""
