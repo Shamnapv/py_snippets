166bee6435b1f4c75ccfa3a63ca362c5f2f2bde4
Shamnapv <161368598+Shamnapv@users.noreply.github.com>
2025-06-08 10:42:09 +0530

    Update task3b.py

--- task3b.py
+++ task3b.py
@@ -19,3 +19,16 @@ for i in range(start_weekday, start_weekday + days_in_month):
     day += 1
 
 print()
+
+"""output:
+(base) PS C:\Users\shaim\Desktop\pythonlab> python task3b.py
+enter the year:2024
+enetr the month(1-12):2
+2024-02-01
+
+Mo Tu We Th Fr Sa Su
+          1  2  3  4
+ 5  6  7  8  9 10 11
+12 13 14 15 16 17 18
+19 20 21 22 23 24 25
+26 27 28 29"""
